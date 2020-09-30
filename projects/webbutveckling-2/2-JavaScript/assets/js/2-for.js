1fecbfdd1eb836aed26cfe197657b87a06b8b958
Marcus Filipsson <macke2002@outlook.com>
2020-09-30 12:01:57 +0200

    prov på js

--- projects/webbutveckling-2/2-JavaScript/assets/js/2-for.js
+++ projects/webbutveckling-2/2-JavaScript/assets/js/2-for.js
@@ -2,7 +2,45 @@ for(var i = 0; i < 10; i++) {
     document.writeln("Count is: " + i + "<br>");
 }
 
+document.writeln("-----------------------------------<br>");
+
 
 for(var count = 1; count < 13; count ++) {
     document.writeln("Count is: " + i + "<br>");
 }
+
+document.writeln("-----------------------------------<br>");
+
+
+var str = "Hello World!";
+
+// document.writeln(str[0]);
+
+for (var i = 0; i < str.length; i++) {
+    document.writeln(str[i]);
+}
+
+document.writeln("<br>-----------------------------------<br>");
+
+
+var codeArr = ["CSS", "HTML", "JS", 33, true]; 
+var len = codeArr.length;
+for(var i =0; i < len; i++) {
+    document.writeln("Language:" + codeArr[i] + "<br>");
+}
+
+document.writeln("-----------------------------------<br>");
+
+for(var i = 1; i < 366; i++) {
+    if(i % 2 === 0) {
+        document.writeln("Even dates: " + i + "<br>");
+    }
+}
+
+
+for(var i = -10; i < 20; i ++) {
+    document.writeln("Count is:" + i + "<br>" );
+}
+
+document.writeln("-----------------------------------<br>");
+
